4dbef6283a135b3796f2ebf6abc7ffbb6d3f511a
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-03-22 21:08:54 +0300

    fix ascii not working

--- ascii.py
+++ ascii.py
@@ -1,15 +1,12 @@
 import sys
 from typing import Tuple, cast
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
 from pathlib import Path
 from PIL import Image
 import io
+import cairosvg
 
 
 def image_to_ascii(image: Image.Image, width: int = 40) -> str:
-    image = image.convert("RGB")
-
     aspect_ratio = image.height / image.width
     height = int(width * aspect_ratio * 0.55)
     image = image.resize((width, height))
@@ -19,13 +16,19 @@ def image_to_ascii(image: Image.Image, width: int = 40) -> str:
     for y in range(image.height):
         row = []
         for x in range(image.width):
-            pixel = cast(Tuple[int, int, int], image.getpixel((x, y)))
-            r, g, b = pixel
+            pixel = cast(Tuple[int, int, int, int], image.getpixel((x, y)))
+            _, _, _, a = pixel
 
-            if not (r > 245 and g > 245 and b > 245):
-                row.append("█")
-            else:
+            if a < 40:
                 row.append(" ")
+            elif a < 100:
+                row.append("░")
+            elif a < 180:
+                row.append("▒")
+            elif a < 230:
+                row.append("▓")
+            else:
+                row.append("█")
 
         ascii_lines.append("".join(row))
 
@@ -33,15 +36,7 @@ def image_to_ascii(image: Image.Image, width: int = 40) -> str:
 
 
 def svg_to_ascii(svg_path: Path, width: int = 40) -> str:
-    drawing = svg2rlg(svg_path)
-    if drawing is None:
-        raise ValueError("Failed to load SVG")
-
-    png_data = renderPM.drawToString(drawing, fmt="PNG")
-    image = Image.open(io.BytesIO(png_data))
+    png_data = cairosvg.svg2png(url=str(svg_path), background_color=None)
+    image = Image.open(io.BytesIO(png_data)).convert("RGBA")
 
     return image_to_ascii(image, width)
-
-
-if __name__ == "__main__":
-    print(svg_to_ascii(sys.argv[1], width=24))
